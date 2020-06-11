24952afcd238c46ce1397bf661c4a9fc78d4deb2
Maria Maciel <mariaeugeniamaciel@gmail.com>
2020-06-11 16:32:13 -0300

    Cria componente de Lista

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Container, Card, Title, Input, Button, Form} from './styles'
 import Header from '../../Components/Header'
-
+import Lista from '../../Components/Lista'
 function Home() {
   return (
     <Container>
         <Header/>
+        <Lista/>
     </Container>
   );
 }
