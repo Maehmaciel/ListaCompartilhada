de4f9c00b75081482da2d156204c72b4b45aaf16
Leonardo Setubal <setubal43@gmail.com>
2020-06-11 21:20:52 -0300

    feat: concluindo header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import {Container} from './styles'
+import { Container, H1, Ul, Li, Link, } from './styles'
+import { FaSearch } from 'react-icons/fa'
 
 function Header() {
-  return (
-    <Container>
-       <span>header</span>
-    </Container>
-  );
+	return (
+		<Container>
+			<H1>Minha Lista Compartilhada</H1>
+
+			{/* <Perfil>Perfil</Perfil>
+			<Compartilhar>Compartilhar</Compartilhar>
+			<FaSearch size={20} color="#141313"></FaSearch> */}
+
+			<Ul>
+				<Li><Link>Perfil</Link></Li>
+				<Li><Link>Compartilhar</Link></Li>
+				<Li><FaSearch size={20} color="#141313" ></FaSearch></Li>
+			</Ul>
+		</Container>
+	);
 }
 
 export default Header;
--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -1,7 +1,41 @@
 import styled from "styled-components"
 
 export const Container = styled.header`
-    width:100%;
+		width:100%;
     height:71px;
-    background-color:#E1E0E0;
+		background-color:#E1E0E0;
+		margin:0 auto;
+		display: flex;
+		flex-wrap:nowrap;
+		align-items:center;
+`
+
+export const H1 = styled.h1`
+	padding: 10px 20px 10px 20px;
+
+	font-family: Share Tech Mono;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 20px;
+	line-height: 23px;
+`
+export const Ul = styled.ul`
+	margin-left: auto;`
+export const Li = styled.li`
+	display: inline-table;
+	padding: 0px 15px 0px 0px
+`
+export const Link = styled.a`
+	display: table;
+	font-size: 16px;
+	padding: 5px 10px;
+	background-image: linear-gradient(to top, #FFD600 0%, #FFD600 100%);
+	background-size: 0% 2px;
+	background-position: bottom;
+	background-repeat: no-repeat;
+	transition: 0.5s;
+	:hover{
+		background-size: 100% 2px;
+		cursor: pointer;
+	};
 `
