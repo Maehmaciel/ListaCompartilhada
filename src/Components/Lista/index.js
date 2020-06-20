c899442ab05cf01d5c9851cb9ce15b14ae545430
Leonardo Setubal <setubal43@gmail.com>
2020-06-20 19:05:31 -0300

    feat: Ajustado alinhamento

--- src/Components/Lista/index.js
+++ src/Components/Lista/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { Container } from './styles'
 
 function Lista() {
-  return (
-    <Container>
-      <div className="dropdown">
-        <button className="dropbtn">Dropdown</button>
-        <div className="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-        </div>
-      </div>
-    </Container>
-  );
+	return (
+		<Container>
+			<div className="dropdown">
+				<button onclick="myFunction()" className="dropbtn">Dropdown</button>
+				<div className="dropdown-content">
+					<ul>
+						<li>
+							Item 1
+						</li>
+						<li>
+							Item 2
+						</li>
+					</ul>
+					<a>Editar</a>
+				</div>
+			</div>
+		</Container>
+	);
 }
 
 export default Lista;
